3d734e351a51faf6ab1307365a39b5e1483cabd5
Tanmoy Debnath <tanmoydebnath2204045@gmail.com>
2025-03-31 18:47:33 +0600

    Fresh Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Directly output to 'build' folder
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
+    outDir: 'build'
   }
-});
+})
