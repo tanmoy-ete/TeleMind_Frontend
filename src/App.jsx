5a04353ee9c51c43e37b049159f8bddfc8c4db97
Tanmoy Debnath <tanmoydebnath2204045@gmail.com>
2026-04-20 23:53:54 +0600

    all_changes1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import { useLanguage } from './context/LanguageContext';
 
